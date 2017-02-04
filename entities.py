b27f46806c1ea009830c9b66ee4d27462a7576bb
jessehogan0 <jessehogan0@gmail.com>
2017-02-04 04:07:57 +0000

    Remove entities.top
    'top' has no precidence in any language I've heard of.

--- entities.py
+++ entities.py
@@ -235,14 +235,6 @@ class entities(object):
     @last.setter
     def last(self, v): self[-1] = v
 
-
-    @property
-    def top(self):
-        """ A synonym of entities.last appropriate when the entities collection
-        is being treated like a LIFO stack.  
-        """
-        return self.last
-
     @property
     def brokenrules(self):
         r = brokenrules()
