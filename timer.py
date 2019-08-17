a7f6b8c09359cd80fcd1a56181e8602a2d08d889
Jesse Hogan <jhogan@carapacian.com>
2019-08-17 17:38:45 +0000

    [orm] Add stopwatch class

---
+++ timer.py
@@ -0,0 +1,37 @@
+from pdb import set_trace; B=set_trace
+import primative
+
+class stopwatch():
+    def __init__(self):
+        self._start = None
+        self._stop = None
+        self.start()
+
+    def start(self):
+        self._start = primative.datetime.now()
+        self._stop = None
+
+    def stop(self):
+        if self._start is None:
+            raise ValueError('Call start before stop')
+
+        self._stop = primative.datetime.now()
+
+    @property
+    def timedelta(self):
+        if self._start is None:
+            raise ValueError('Stop watch has not been started')
+
+        stop = self._stop or primative.datetime.now()
+        return stop - self._start
+
+    @property
+    def milliseconds(self):
+        return int(self.timedelta.total_seconds() * 1000)
+
+    def __str__(self):
+        return str(self.timedelta)
+        
+        
+        
+        
