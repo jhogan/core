0760c81cee69c251155fec3eba0a1d77f5c70045
Jesse Hogan <jhogan@carapacian.com>
2019-08-24 14:34:41 +0000

    [web] Begin web.py
    * Added web.site, header, footer, page, element, paragraph, text, strong
      and span
    * Added tests
    * Formated comments

---
+++ web.py
@@ -0,0 +1,169 @@
+# vim: set et ts=4 sw=4 fdm=marker
+
+#######################################################################r
+# Copyright (C) Jesse Hogan - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited
+# Proprietary and confidential
+# Written by Jesse Hogan <jessehogan0@gmail.com>, 2019
+########################################################################
+
+import entities
+import textwrap
+import orm
+from dbg import B
+
+class site(entities.entity):
+    def __init__(self, name):
+        self.name = name
+        self.pages = pages()
+        self.headers = headers()
+
+    def header(self):
+        return self.headers.default
+
+class components(entities.entities):
+    pass
+
+class component(entities.entity):
+    def __init__(self):
+        self.menus = menus()
+
+class headers(components):
+    pass
+
+class header(component):
+    def __init__(self):
+        self.logo = None
+        self.searchbox = None
+        self.notifications = None
+
+class footers(components):
+    pass
+
+class footer(component):
+    # Reference:
+    # https://www.orbitmedia.com/blog/website-footer-design-best-practices/
+    def __init__(self):
+        self.copyright = copyright()
+    
+    def sitemap(self):
+        # Derived or overridden
+        pass
+
+    def privacypolicy(self):
+        raise NotImplementedError('Author must override') 
+
+    def termsofuse(self):
+        raise NotImplementedError('Author must override') 
+
+    def contact(self):
+        # Derived or overridden
+        pass
+
+    def address(self):
+        # Derived or overridden
+        pass
+
+    def phonenumbers(self):
+        # Derived or overridden
+        pass
+
+    def socialmedia(self):
+        # Derived or overridden
+        pass
+
+class header(component):
+    pass
+
+class pages(entities.entities):
+    pass
+
+class page(entities.entity):
+    def __init__(self, name):
+        self.pages = pages()
+
+class elements(entities.entities):
+    pass
+
+class element(entities.entity):
+    def __init__(self, str=None):
+        if str is not None:
+            self.elements += text(str)
+
+    @property
+    def elements(self):
+        if not hasattr(self, '_elements'):
+            self._elements = elements()
+        return self._elements
+
+    @elements.setter
+    def elements(self, v):
+        self._elements = v
+
+    def __iadd__(self, el):
+        if type(el) is str:
+            el = text(el)
+
+        if not isinstance(el, element):
+            raise ValueError('Invalid element type: ' + str(type(el)))
+
+        self.elements += el
+        return self
+
+    @property
+    def tag(self):
+        return type(self).__name__
+
+    @property
+    def html(self):
+
+        body = str()
+        if isinstance(self, text):
+            body = str(self)
+
+        for i, el in enumerate(self.elements):
+            if i: body += '\n'
+            body += textwrap.indent(el.html, '  ')
+
+        if isinstance(self, text):
+            return body
+
+        return '<%s>\n%s\n</%s>' % (self.tag, body, self.tag)
+
+    def __repr__(self):
+        B()
+        return str(self)
+
+class paragraphs(elements):
+    pass
+
+class paragraph(element):
+    def __init__(self, body=None, *args):
+        if body is not None:
+            if type(body) is str:
+                body %= args
+            elif args:
+                raise ValueError('No args allowed')
+
+            self += body
+
+    @property
+    def tag(self):
+        return 'p'
+
+class text(element):
+    def __init__(self, str):
+        self._str = str
+
+    def __str__(self):
+        return textwrap.dedent(self._str).strip()
+
+class strong(element):
+    pass
+
+class span(element):
+    pass
+
+    
+    
