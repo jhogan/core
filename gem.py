030a12817e21a43e62f82467d467bd916e7d8681
Jesse Hogan <jhogan@carapacian.com>
2019-11-02 15:24:39 +0000

    [gem] Modified gem entities

--- gem.py
+++ gem.py
@@ -6,16 +6,48 @@
 # Proprietary and confidential
 # Written by Jesse Hogan <jessehogan0@gmail.com>, 2019
 
+# TODO Checkout http://wiki.goodrelations-vocabulary.org/Quickstart for
+# an standard for ecommerce markup when marking up parties, places, etc.
+
 import orm
-from primative import datetime
-
-class partytypes(orm.entities):
-    pass
-
-class partytype(orm.entity):
-    pass
+import primative
+from datetime import datetime
+from dbg import B
 
 class parties(orm.entities):
+    pass
+
+class organizations(parties):
+    pass
+
+class party_parties(orm.associations):
+    pass
+
+class party_addresses(orm.associations):
+    pass
+
+class persons(parties):
+    pass
+
+class address_regions(orm.associations):
+    pass
+
+class addresses(orm.entities):
+    pass
+
+class regions(orm.entities):
+    pass
+
+class party_contactmechanisms(orm.associations):
+    pass
+
+class contactmechanisms(orm.entities):
+    pass
+
+class phones(contactmechanisms):
+    pass
+
+class emails(contactmechanisms):
     pass
 
 class party(orm.entity):
@@ -36,10 +68,7 @@ class party(orm.entity):
     # The International Standard of Industrial Classification of All
     # Economic Activities (ISIC), Revision 4 code for a particular
     # organization, business person, or place.
-    isicv4 = char, min=1, max=1
-
-class organizations(parties):
-    pass
+    isicv4 = str, 1, 1
 
 class organization(party):
     """ Represents a group of people with a common purpose. Examples
@@ -48,10 +77,6 @@ class organization(party):
     organizations may include teams, families, etc.
     """
     name = str
-
-
-class persons(parties):
-    pass
 
 class person(party):
     firstname      =  str
@@ -66,11 +91,146 @@ class person(party):
     # TODO
     # passport = passport
 
-class partyclassifications(orm.associations):
+class region(orm.entity):
+    Postalcode  =  0
+    County      =  1
+    City        =  2
+    State       =  3
+    Province    =  4
+    Territory   =  5
+    Country     =  6
+
+    """ An instance of a geographical region. Geographical regions
+    include postal codes, cities, counties, states, provinces,
+    territories and countries. 
+
+    This recursive entity. The parent-child relationship implies a
+    _within_ relationship. That is to say: all regions are
+    geographically within their parent. The inverse is also true: that
+    all child regions are geographically within a given region.
+    """
+    # TODO Automatically pluralize
+    entities = regions
+
+    # The main string representation of the region. For postal codes, it
+    # will be the postal code itself, e.g., "86225". For countries, the
+    # name will be the name of the country, e.g., "United States".
+    name = str
+
+    # The direct child regions. Country regions will have state or
+    # province regions.  City regions will have postal code regions, and
+    # so on.
+    regions = regions
+
+    # A numeric code to indicate the region type 
+    type = int
+
+    abbreviation = str
+
+class contactmechanism(orm.entity):
+    """ An abstract class representing a mechanism through which a party
+    or facility can be contacted. Subtypes include ``phone``, ``email``,
+    ``address`` and so on.
+    """
+
+class phone(contactmechanism):
+    """ Represents a phone number contact mechanism.
+    """
+
+    # The area code as defined by the North American Numbering Plan.
+    # This should probably be None for phone numbers outside of North
+    # America and the Caribean. NOTE many of the numbers within the 200,
+    # 999 range are currently considered invalid. The should probably
+    # result in `brokenrules` if given. See
+    # https://en.wikipedia.org/wiki/List_of_North_American_Numbering_Plan_area_codes
+    areacode = int, 200, 999
+
+    # The phone number excluding the country code, area code and
+    # extension. (See `party_contactmechanism` for `extension`.)
+    number = str
+
+class email(contactmechanism):
     pass
 
-class partyclassification(orm.association):
-    partytype  =  partytype
-    party      =  party
-    from       =  datetime
-    to         =  datetime
+class party_contactmechanism(orm.association):
+    # The date range through which this contactmechanism applied to the
+    # given ``party``.
+    begin          =  datetime
+    end            =  datetime
+
+    # If True, indicates that the mechanism may be called for
+    # solicitation purposes. If False, the mechanism may not be called
+    # for solicitation purposes. It will default to `None` meaning that
+    # no preference on solicitation has been given, which is
+    # functionally equivalent to True.
+    solicitations  =  bool
+    
+    # TODO Multiple comments should be made about the contact mechanism.
+    # Ensure the `comment` entity can polymorphically record comments on
+    # contactmechanism types
+
+    # The line extension
+    extension = str
+
+    # The reflective association properties, i.e.,
+    # party-to-contactmechanism
+    party = party
+    contactmechanism = contactmechanism
+
+    # The purpose that the contactmechanism is currently being used by
+    # the party. (Exmples: 'mobile', 'main fax', 'general',
+    # 'headquarters', 'billing inquiries', 'sales office', 'central
+    # internet address', 'service address', 'main office number', 'main
+    # home number', 'secondary office number', 'work email address',
+    # 'personal email address', 'main home address')
+    purpose = str
+
+class address(orm.entity):
+    """ A postal address.
+    """
+
+    # TODO Automatically pluralize
+    entities = addresses
+
+    # Address line 1
+    address1 = str
+
+    # Address line 2
+    address2 = str
+
+    # A reference to a geographical region. This will usually be a
+    # postal code region from which the other regions (city, state,
+    # etc.) may be obtained.
+    regions = regions
+
+    # Prosaic directions to the address
+
+    # FIXME The following line results in an exception from MySQL:
+    #     _mysql_exceptions.OperationalError: (1074, "Column length too
+    #     big for column 'directions' (max = 16383); use BLOB or TEXT
+    #     instead")
+    #directions = str, 1, 65535
+    directions = str, 1, 65536
+
+class address_region(orm.association):
+    """ An association between a postal address (``address``) and a
+    geographic region (``region``).
+    """
+    address = address
+    region = region
+
+class party_address(orm.association):
+    """ An association between a party (person or organization) and a
+    postal address.
+    """
+    entities  =  party_addresses
+    party     =  party
+    address   =  address
+    begin     =  datetime
+    end       =  datetime
+
+class party_party(orm.association):
+    entities = party_parties
+    subject  =  party
+    object   =  party
+    role     =  str
