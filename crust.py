6c03ef4d7b9439381f9f286c22a32b940ca1adf3
Jesse Hogan <jhogan@carapacian.com>
2020-07-30 06:08:13 +0000

    [mig] Continue objectifying mig()

--- crust.py
+++ crust.py
@@ -39,134 +39,168 @@ def wf(file, txt):
     return p.write_text(txt)
 
 class undef: pass
-def ask(msg, yesno=True, default=undef, **kwargs):
-    res = None
-    while not res:
-        res = input(f'{msg} ').lower()
-        if not res and default is not undef:
-            return default
-
-    if yesno:
-        if res in ('y', 'yes'):
-            return 'yes'
-        elif res in ('n', 'no'):
-            return 'no'
-
-    for k, v in kwargs.items():
-        if res == v:
-            return k
-
-    return res
 
 class process:
-    def say(msg):
+    class Abort(Exception): pass
+
+    def say(self, msg):
         print(msg)
+
+    def ask(self, msg, yesno=True, default=undef, **kwargs):
+        res = None
+        while not res:
+            res = input(f'{msg} ').lower()
+            if not res and default is not undef:
+                return default
+
+        if yesno:
+            if res in ('y', 'yes'):
+                return 'yes'
+            elif res in ('n', 'no'):
+                return 'no'
+
+        for k, v in kwargs.items():
+            if res == v:
+                return k
+
+        return res
     
 class mig(process):
     def __init__(self):
         self._done = orm.ormclasseswrapper()
         self._todo = orm.ormclasseswrapper()
         self._isscaned = False
+        self._tmp = None
+        try:
+            self()
+        except process.Abort:
+            pass
+        except:
+            raise
+        finally:
+            self.destructor()
+    
+    def destructor(self):
+        if self._tmp is not None:
+            os.remove(self._tmp)
 
-    class exec(self):
-        def run(self):
+    @property
+    def tmp(self):
+        # Create a tmp file
+        if not self._tmp:
+            _, self._tmp = tempfile.mkstemp()
+        return self._tmp
+
+    @property
+    def editor(self):
+        # Get an editor that the user likes
+        return os.getenv('EDITOR') or '/usr/bin/vim'
+
+    def exec_alter_table(self, e):
+        try:
+            at = e.orm.altertable
+            #MESSUP THE ALTERTABLE
+
+
+
+
+            at = 'xxx' + at
+
+            orm.orm.exec(at)
+        except Exception as ex:
+            self.say(f'\n{ex}\n\n')
+            res = self.ask(
+                'Press [e]dit, [i]gnore or [q]uit...',
+                edit='e', ignore='i', quit='q'
+            )
+            if res == 'edit':
+                self.exec_edited_alter_table(e)
+
+            elif res == 'ignore':
+                return
+            elif res == 'quit':
+                raise self.Abort()
+
+    def exec_edited_alter_table(self, e):
+        at = None
+        while True:
             # Execute the edited ALTER TABLE statement
+            at = self.edit_alter_table(e, at)
+
             try:
                 orm.orm.exec(at)
             except Exception as ex:
-                say(f'\n{ex}\n\n')
-                res = ask(
-                    'Press re[e]dit, [i]gnore or [q]uit...',
+                self.say(f'\n{ex}\n\n')
+                res = self.ask(
+                    'Press [e]dit, [i]gnore or [q]uit...',
                     edit='e', ignore='i', quit='q'
                 )
                 if res == 'ignore':
-                    break
-                elif res == 'quit':
                     return
+                elif res == 'quit':
+                    raise process.Abort()
             else:
                 break
 
-    class all(process):
-        def run(self):
-            ddl = str()
-            for j, e in enumerate(es[i:]):
-                at = e.orm.altertable
-                ddl += f'{at}\n\n'
+    def process_all(self):
+        ddl = str()
+        for j, e in enumerate(es[i:]):
+            at = e.orm.altertable
+            ddl += f'{at}\n\n'
 
-            res = ask(f'{ddl}\nExecute the above DDL? [Yn]')
-            if res == 'yes':
-                say('executing...')
-                return 'done'
-            elif res == 'no':
-                return 'abort'
-    class edit(process):
-        def __init__(self):
-            self._tmp = None
+        res = ask(f'{ddl}\nExecute the above DDL? [Yn]')
+        if res == 'yes':
+            say('executing...')
+            return 'done'
+        elif res == 'no':
+            return 'abort'
 
-        @property
-        def tmp(self):
-            # Create a tmp file
-            if not self._tmp:
-                _, self._tmp = tempfile.mkstemp()
-            return self._tmp
+    def edit_alter_table(self, e, at=None):
+        if not at:
+            at = e.orm.altertable
+            tbl = e.orm.migration.table
+            at = (
+                f'{at}\n\n'
+                f'/* Model-to-table comparison: \n{tbl}\n*/'
+            )
+        
+        # Write the ALTER TABLE to the tmp file
+        with open(self.tmp, 'w') as f:
+            f.write(at)
 
-        @property
-        def editor(self):
-            # Get an editor that the user likes
-            editor = os.getenv('EDITOR') or '/usr/bin/vim'
+        flags = ''
+        basename = os.path.basename(self.editor)
+        if basename in ('vi', 'vim'):
+            flags = '-c "set syn=sql"'
+        if flags:
+            flags = f' {flags} '
 
-        def edit(self):
-            basename = os.path.basename(self.editor)
-            
-            flags = ''
-            if basename in ('vi', 'vim'):
-                flags = '-c "set syn=sql"'
-            if flags:
-                flags = f' {flags} '
+        os.system(f'{self.editor}{flags}{self.tmp}')
 
-            os.system(f'{editor}{flags}{tmp}')
+        # Read back in the edited file
+        with open(self.tmp, mode='r') as f:
+            at = f.read()
 
-        def run(self):
-            # Write the ALTER TABLE to the tmp file
-            with open(tmp, 'w') as f:
-                f.write(
-                    f'{at}\n\n'
-                    f'/* Model-to-table comparison: \n{tbl}\n*/'
-                )
-
-            self.edit()
-
-            # Read back in the edited file
-            with open(tmp, mode='r') as f:
-                at = f.read()
-
-            self.processes.process('exec', at)
-
-
-
-    def usage():
-        print(textwrap.dedent("""
-        y - yes, apply DDL
-        n - no, do not apply DDL
-        q - quit migration
-        a - apply this DDL and all later DLL
-        e - manually edit the current DDL
-        h - print help
-        """))
+        return at
 
     def scan(self):
-        tmp = None
         if not self._isscaned:
-            self._todo += orm.migration().entities
-
             self.say('Scanning for entities to migrate ...\n')
+
+            try:
+                self._todo += orm.migration().entities
+            except:
+                raise
+            finally:
+                self._isscaned = True
+
 
             es = self.todo
             if es.count:
                 self.say(f'There are {es.count} entities to migrate:')
 
             for e in es:
-                say(f'    - {e.__module__}.{e.__name__}')
+                self.say(f'    - {e.__module__}.{e.__name__}')
 
     @property
     def done(self):
@@ -174,18 +208,28 @@ class mig(process):
 
     @property
     def todo(self):
-        scan()
+        self.scan()
         return self._todo
 
-    def run():
-        start = ask(
+    def __call__(self):
+        def usage():
+            print(textwrap.dedent("""
+            y - yes, apply DDL
+            n - no, do not apply DDL
+            q - quit migration
+            a - apply this DDL and all later DLL
+            e - manually edit the current DDL
+            h - print help
+            """))
+
+        start = self.ask(
             '\nWould you like to start [Yn]?', yesno=True, default='yes'
         )
 
         if start != 'yes':
             return
 
-        scan()
+        self.scan()
 
         es = self.todo
         for i, e in es.enumerate():
@@ -194,7 +238,7 @@ class mig(process):
             print(f'{tbl!s}\n{at}\n')
 
             while True:
-                res = ask(
+                res = self.ask(
                     'Apply this DDL [y,n,q,a,e,h]?', yesno=True, 
                     quit='q', all='a', edit='e', help='h'
                 )
@@ -205,20 +249,21 @@ class mig(process):
                 if res == 'help':
                     usage()
 
-            if res == 'all':
-                res = self.processes.process('all')
+            if res == 'yes':
+                self.exec_alter_table(e)
+            elif res == 'no':
+                continue
+            elif res == 'quit':
+                B()
+                raise process.Abort()
+            elif res == 'all':
+                res = self.process_all()
                 if res == 'abort':
                     continue
-            elif res == 'quit':
-                return
 
             elif res == 'edit':
                 res = self.processes.process('edit')
-    except Exception as ex:
-        print(f'\n{ex}\n')
-    finally:
-        if tmp:
-            os.remove(tmp)
+
 
 cfg = config()
 acct = db.connections.getinstance().default.account
