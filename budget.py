8e309ea972b26057b4fdcb571ef8ca463fd7ed04
Jesse Hogan <jhogan@carapacian.com>
2020-08-25 06:29:47 +0000

    [gem] Add budget revision classes

--- budget.py
+++ budget.py
@@ -69,6 +69,23 @@ class budget(orm.entity):
 
     periods = periods
 
+    # Making the ``budget`` entity recursive is a simple way to model
+    # budegt revisions.
+    #
+    # Budget revisions may simply be handled by creating and
+    # maintainting a whole new budeget each time a revision is needed.
+    # The old budget can be relate to the next budget via this recursive
+    # relationship. While simple, the disadvantage is that the whole
+    # budget needs to be rerecorded, even though many of the budgeted
+    # items may remain consisistent from one revision to the next. 
+    #
+    # For a more robust vay of revisioning budgets, see the
+    # ``revision`` entity.
+    budgets = budgets
+
+    # A collection of budget revisions
+    revisions = revisions
+
 class operating(budget):
     """ A budget subtype for expense items.
 
@@ -157,6 +174,9 @@ class item(orm.entity):
     # allowing for a hierarchy of budget item rollup.
     items = items
 
+    # A collection of budget revisions
+    revisions = revisions
+
 class itemtype(orm.entity):
     """ Classifies the budegtary ``item``s into types so that common
     budget item descriptions can be reused.
@@ -221,3 +241,47 @@ class periodtype(orm.entity):
 
     # The collection of budetary ``periods``.
     periods = periods
+
+class revision(orm.entity):
+    """ A budget may have one or more budget ``revisions`` over time,
+    which can affect many parts of the ``budget``. Each budget
+    ``revision`` may affect more than one budget ``item`` and vice
+    versa, thus resulting in the many-to-many relationship between
+    budget ``items`` and budget ``revisions``, resolved by the budget
+    revision ``impacts``, which is an associative entity.
+
+    Note that this entity was originally called BUDGET REVISION in "The Data
+    Model Resource Book".
+    """
+
+    # The date the budget was revised
+    revised = date
+
+class impact(orm.association):
+    """  Each budget ``item`` may be affected by one or more budget
+    revision ``impacts``. 
+
+    Note that this entity was originally called BUDGET REVISION IMPACT
+    in "The Data Model Resource Book".
+    """
+
+    # The budget ``item`` side of the association
+    item = item
+
+    # The budget ``revision`` side of the association
+    revision = revision
+
+    # The **revised amount** maintains the reduction or increased amount
+    # of a budgeted ``item``.
+    amount = dec
+
+    # Indicates the item has been added to the budet by the budget
+    # revision.  If the bool is False, the item would be considered
+    # ``issubtractive``.
+    #
+    # Note that the book calls this the **add delete flag**
+    # added or deleted according to the budget ``revision``.
+    isadditive = bool
+
+    # Shows why each budgeted item needed to be changed.
+    reason = text
