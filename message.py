c80413f5e0afc0ea85d38c91574ad3dca1d834c6
Jesse Hogan <jhogan@carapacian.com>
2021-03-10 06:35:19 +0000

    Add dispatch class to message.py
    On branch main

--- message.py
+++ message.py
@@ -15,18 +15,32 @@ TODOs:
 """
 from datetime import datetime, date
 from func import B
-import apriori
 from orm import text
+import apriori
 import orm
 import party
+import textwrap
+from uuid import uuid4
 
 class messages(orm.entities):
+    pass
+
+class dispatches(orm.entities):
     pass
 
 class contactmechanism_messages(orm.associations):
     pass
 
 class contactmechanism_messagetypes(apriori.types):
+    pass
+
+class statuses(orm.entities):
+    pass
+
+class statustypes(apriori.types):
+    pass
+
+class dispatchtypes(apriori.types):
     pass
 
 class message(orm.entity):
@@ -84,8 +98,21 @@ class contactmechanism_message(orm.association):
     contactmechanism = party.contactmechanism
     message = message
 
+class dispatch(orm.entity):
+    statuses = statuses
 
+class dispatchtype(apriori.type):
+    """ Email, sms, etc.
+    """
+    dispatches = dispatches
+    
+class status(orm.entity):
+    begin = datetime
+    dispatches = dispatches
 
-
-
-
+class statustype(apriori.type):
+    """ Records the type of status. Typical values for the ``name``
+    attribute include: "sending", "sent" and "viewed".
+    """
+    # The collection of statuses belonging to this type
+    statuses = statuses
