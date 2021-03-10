20b9679323c72a62a53ae5a19b2bf56ff458b610
Jesse Hogan <jhogan@carapacian.com>
2021-03-10 06:32:58 +0000

    Start tests for Postmark; add third.py
    On branch bot

---
+++ testthird.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# vim: set et ts=4 sw=4 fdm=marker
+
+########################################################################
+# Copyright (C) Jesse Hogan - All Rights Reserved                      #
+# Unauthorized copying of this file, via any medium is strictly        #
+# prohibited                                                           #
+# Proprietary and confidential                                         #
+# Written by Jesse Hogan <jessehogan0@gmail.com>, 2021                 #
+########################################################################
+
+from func import enumerate, B
+import third
+import message
+import tester
+import orm
+import ecommerce
+import party
+
+class test_postmark(tester.tester):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        orm.security().override = True
+
+        if self.rebuildtables:
+            es = orm.orm.getentitys(includeassociations=True)
+            for e in es:
+                if e.__module__ in ('third', 'message'):
+                    e.orm.recreate()
+
+        # Create an owner and get the root user
+        own = ecommerce.user(name='hford')
+        root = ecommerce.users.root
+
+        # Save the owner, the root user will be the owner's owner.
+        with orm.sudo():
+            own.owner = root
+            own.save()
+
+        # Going forward, `own` will be the owner of all future records
+        # created.
+        orm.security().owner = own
+
+        # Create a company to be the propritor.
+        com = party.company(name='Ford Motor Company')
+        com.save()
+
+        # Set the company as the proprietory
+        orm.security().proprietor = com
+
+        # Update the owner (hford) so that the company (Ford Motor
+        # Company) is the proprietor.
+        own.proprietor = com
+        own.save()
+
+    def it_sends(self):
+        msg = message.message.email(
+            from_    =  'source@example.com',
+            to       =  'destination@example.com',
+            subject  =  'Test email',
+            text     =  'Test message',
+            html     =  '<p>Test message</p>',
+        )
+
+        dis = msg.dispatch(
+            dispatchtype = message.dispatchtype(name='email')
+        )
+
+        pm = third.postmark()
+
+        res = pm.send(dis)
+
+if __name__ == '__main__':
+    tester.cli().run()
---
+++ third.py
@@ -0,0 +1,85 @@
+# vim: set et ts=4 sw=4 fdm=marker
+
+########################################################################
+# Copyright (C) Jesse Hogan - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited
+# Proprietary and confidential
+# Written by Jesse Hogan <jessehogan0@gmail.com>, 2021
+########################################################################
+
+""" This module contains all classes related to third party network
+API's.
+
+TODOs:
+"""
+
+from func import enumerate, B
+import product
+
+class internetservices(product.services):
+    pass
+
+class apis(internetservices):
+    pass
+
+class internetservice(product.service):
+    pass
+
+class mails(apis):
+    pass
+
+class postmarks(mails):
+    pass
+
+class api(internetservice):
+    # The secret API KEY
+    key = str
+
+    # Credentials
+    username = str
+    password = str
+
+    host = str
+    port = int
+
+    @property
+    def browser(self):
+        if not self._browser:
+            self._browser = www.browser()
+
+            self.browser.onbeforerequest += \
+                self._browser_onbeforerequest
+
+        return self._browser
+
+    def _browser_onbeforerequest(self, src, eargs):
+        B()
+
+class mail(api):
+    def send(self):
+        raise NotImplementedError(
+            'Must be implemented by a subentity'
+        )
+
+class postmark(mail):
+    """ A transactional email provider.
+
+    URL: https://postmarkapp.com
+    """
+    def send(self, dis):
+        """
+
+        :param: dis message.delevery: The message delevery entity to
+        send. Note that the delivery object will have the actual message
+        entity that we are sending.
+        """
+        tab = self.browser.tab()
+
+        msg = dis.message
+
+        # TODO Convert msg to JSON
+
+        res = tab.post()
+
+        
