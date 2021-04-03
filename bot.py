1d4da39cd3c6ab1a88a907618649e997bcefb78d
Jesse Hogan <jhogan@carapacian.com>
2021-04-03 07:44:57 +0000

    Develop sendbot._dispatch
    On branch bot

--- bot.py
+++ bot.py
@@ -20,8 +20,54 @@ class bot(ecommerce.agent):
         raise NotImplementedError('Implement in subentity')
 
 class sendbots(bots):
-    ...
+    pass
 
 class sendbot(bot):
-    ...
+    def __call__(self):
+        while True:
+            self._dispatch()
+
+    def _dispatch(self)
+        diss = message.dispatches('completed = %s', None)
+
+        for dis in diss:
+            emailer = third.emailer.create()
+
+            if dis.dispatchtype.name != 'email':
+                # TODO We will want to get an SMS services for
+                # dispatchtypes of SMS. Same for mail, or any other
+                # dispatch type. 
+                
+                # TODO We should probably log when there are unsupported
+                # dispatch types. Sendbot shouldn't press on however.
+
+                continue
+
+            emailer.send(dis)
+
+            try:
+                pm.send(dis)
+            except third.api.Error as ex:
+                # Get the emailer service's code and message for the
+                # error. This is distinct from the HTTP status code and
+                # reason/phrase.
+                code = ex.code
+                msg  = ex.message
+
+            # TODO We may want to catch network errors here. For
+            # example, if the network is down, we may want to give up
+            # for the moment.
+            except Exception as ex:
+                # TODO Log and continue
+
+
+            
+
+
+
+
+            
+
+
+
 
