b72077a5c3a4dc88f5e27790fe6840512f85869e
Jesse Hogan <jhogan@carapacian.com>
2021-05-14 06:42:17 +0000

    Remove party.retrievability
    It wasn't needed at the moment afterall.
    
    On branch bot-invoking-and-reporting

--- party.py
+++ party.py
@@ -226,14 +226,6 @@ class party(orm.entity):
 
         return ents.first
 
-    @property
-    def retrievability(self):
-        usr = orm.security().user
-        B()
-        if usr.party:
-            return orm.violations.empty
-
-        
 class organization(party):
     """ An abstract class representing a group of people with a common
     purpose. Subtypes may include schools, NGOs, clubs, corporations,
