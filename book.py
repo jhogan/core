68ef56ac62fde641b5c71e7d57f93d48be040b36
Jesse Hogan <jhogan@carapacian.com>
2022-05-12 08:25:57 +0000

    Setup API
    On branch book

---
+++ book.py
@@ -0,0 +1,23 @@
+# vim: set et ts=2 sw=2 fdm=marker
+
+# Copyright (C) Jesse Hogan - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited
+# Proprietary and confidential
+# Written by Jesse Hogan <jessehogan0@gmail.com>, 2022
+
+with book(name='Hacking Carapacian Core'):
+  with chapter("Introduction"):
+    with section(no=0, name="Introduction") as sec:
+      
+      sec('''
+      It was the best of times, it was the worst of times.
+      ''')
+
+    with section(no=1, name="Testing") as sec:
+      
+      sec('''
+      Testing is a boring part of the developer's journey.
+      ''')
+    
+    
