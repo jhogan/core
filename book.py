19d5fe65aabd1e765df3ed23c8f30488eeaecaed
Jesse Hogan <jhogan@carapacian.com>
2022-05-15 08:53:36 +0000

    Add tester introduction
    On branch book

--- book.py
+++ book.py
@@ -8,15 +8,36 @@
 
 with book('Hacking Carapacian Core'):
   with chapter("Introduction"):
-    with section('What is Carapacian Core') as sec:
-      sec('''
+    with section('What is Carapacian Core'):
+      print('''
         Carapacian core is a framework written and maintained to
         facilitate the creation of web application that deal with
         business data.
       ''')
 
-  with chapter("Writing tests") as sec:
-    ...
+  with chapter("Writing tests"):
+    with section("Introduction"):
+      print('''
+        Whether you're adding a feature to the ORM, creating a nwe web
+        page, or writing backend code to interact with a third-party
+        service, everything in the framework begins and ends with a
+        battery of automated tests.
+
+        Virtually everything gets a test in the framework from the
+        simple to the complex, so it makes sense to start with a
+        discussion on writting tests. 
+      ''')
+
+    with section("tester"):
+      print("""
+        Included with the framework is a module called, simply enough,
+        `tester.py`. It has most of the features that you may be familiar
+        with from other test framworks, such as Python's builtin
+        `unittest`. In addition to the standard assert methods, it
+        includes a `browser` for the purpose of running tests against
+        websites, a `benchmark` subclass for performance testing, and
+        other useful, framework specific features.
+      """)
 
   with chapter("Using the Object-relational Mapper") as sec:
     ...
