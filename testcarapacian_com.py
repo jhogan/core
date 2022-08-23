4ee93c253eda6077153ce16e8ebeb85af3825ac1
Jesse Hogan <jhogan@carapacian.com>
2022-08-23 08:05:02 +0000

    Add tests to testcarapacian_com.py
    On branch reify

--- testcarapacian_com.py
+++ testcarapacian_com.py
@@ -13,6 +13,7 @@ from uuid import uuid4, UUID
 import carapacian_com
 import tester
 import pom
+import orm
 import party
 
 class sites(tester.tester):
@@ -21,10 +22,57 @@ class sites(tester.tester):
         self.isinstance(wss, pom.sites)
 
 class site(tester.tester):
+    def it_has_correct_id(self):
+        # Set constant
+        Id = UUID(hex='c0784fca-3fe7-45e6-87f8-e2ebbc4e7bf4')
+
+        # Test class-level constant
+        self.eq(Id, carapacian_com.site.Id)
+
+        # Test instance
+        self.eq(Id, carapacian_com.site().id)
+
+    def it_has_correct_proprietor(self):
+        cara = party.company.carapacian
+        self.is_(cara, carapacian_com.site().Proprietor)
+
+    def it_has_correct_host(self):
+        Host = 'carapacian.com'
+        self.eq(Host, carapacian_com.site().host)
+
+    def it_has_correct_title(self):
+        Title = 'Carapacian Sustainable Software'
+
+        ws = carapacian_com.site()
+
+        self.eq(Title, ws.title)
+
     def it_has_carapacian_as_its_proprietor(self):
         ws = carapacian_com.site()
         self.eq(ws.proprietor.id, party.company.carapacian.id)
-        
+
+''' Page tests '''
+
+class home(tester.tester):
+    def it_call_name(self):
+        pg = carapacian_com.home()
+        self.eq('index', pg.name)
+
+    def it_GETs(self):
+        ws = carapacian_com.site()
+        tab = self.browser().tab()
+
+        with orm.proprietor(party.company.carapacian):
+            res = tab.get('/', ws)
+        self.status(200, res)
+
+        self.eq(f'{ws.title} | Index', res['html>head>title'].text)
+
+    def it_has_correct_title(self):
+        tab = self.browser().tab()
+        res = tab.get('/', ws)
+        self.startswith(Title, res['html>head>title'].text)
+
 
 if __name__ == '__main__':
     tester.cli().run()
